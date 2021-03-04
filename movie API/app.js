93b457d0dd685b6a9341020b594bfeaf04c6c84e
surjit <surjitprafullit@gmail.com>
2021-03-04 18:20:29 +0530

    connected to DB, login and register routes created

--- movie API/app.js
+++ movie API/app.js
@@ -1,3 +1,4 @@
+//Requiring all the modules/dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongodb = require ('mongodb')
@@ -5,14 +6,19 @@ const path = require('path');
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
-const port = 27017
-const app = express()
+const port = 27017                          //defining the port
+const app = express()                       //initialising express
+app.use(bodyParser.json({ extended: false }));
+
+//connecting to database
 mongoose.connect("mongodb://localhost:27017/project", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false, 
     useCreateIndex: true
 } )
+
+// defining user and movie schema
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
@@ -27,8 +33,37 @@ const movieSchema = new mongoose.Schema({
     genre: String
 })
 
-// url = 'mongodb://127.0.0.1:27017'
+// creating collections by mongoose model
+const User = new mongoose.model('User', userSchema)
+const Movie = new mongoose.model('Movie', userSchema)
 
+//defining login and register routes
+app.post('/register', (req,res)=>{
+    bcrypt.hash(req.body.password, 10,function(err, hash){
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            is_admin: req.body.is_admin   
+        })
+})
+    res.send("user data stored")
+})
+
+app.post('/login',(req, res)=>{
+    User.findOne({email: req.body.email},(err,user)=>{
+        if(!user) return res.send("No user found, register first!")
+        else{
+            const passIsValid = bcrypt.compare(req.body.password, user.password)
+            if (!passIsValid) return res.send("Invalid password")
+            else{
+                res.send(`Welcome ${user.name}`)
+            }
+        }
+})
+})
+
+//creating server
 app.listen(port,()=>{
     console.log(`Listening to port http://localhost:${port}`)
 })
