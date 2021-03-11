8380a6381aed00acd55dde2a18c7c903982aa8c7
surjit <surjitprafullit@gmail.com>
2021-03-12 00:23:35 +0530

    connected to atlas

--- movie_API/config/db.js
+++ movie_API/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/project", { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false, 
-    useCreateIndex: true
-})
+mongoose.connect('mongodb+srv://surjit:surjit@cluster0.0k5cr.mongodb.net/Project?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+// mongoose.connect("mongodb://localhost:27017/project", { 
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true, 
+//     useFindAndModify: false, 
+//     useCreateIndex: true
+// })
